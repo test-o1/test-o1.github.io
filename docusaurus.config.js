ed1a96d7147ed95449ace9bf70cdd6b67970f7fb
Test Alp <test@alpcoker.com>
2022-08-01 16:40:45 +0300

    favicon, doc edit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/test-o1/test-o1.github.io/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
